a238c6fe5ef5234415b1882dd71eb08013276adb
Dmitry Tsepaev <d.tsepaev@lichi.me>
2026-02-20 19:50:58 +0300

    update producer consumer

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -14,14 +14,14 @@ import (
 	"time"
 )
 
-func producer(stream Stream, tweets chan *Tweet, quit chan bool, wg *sync.WaitGroup) {
+func producer(stream Stream, tweets chan *Tweet, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for {
 		tweet, err := stream.Next()
 
 		if err == ErrEOF {
-			quit <- true
+			close(tweets)
 			return
 		}
 
@@ -29,36 +29,31 @@ func producer(stream Stream, tweets chan *Tweet, quit chan bool, wg *sync.WaitGr
 	}
 }
 
-func consumer(tweets chan *Tweet, quit chan bool, wg *sync.WaitGroup) {
+func consumer(tweets chan *Tweet, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for {
-		select {
-		case t := <-tweets:
-			if t.IsTalkingAboutGo() {
-				fmt.Println(t.Username, "\ttweets about golang")
-			} else {
-				fmt.Println(t.Username, "\tdoes not tweet about golang")
-			}
-		case <-quit:
-			return
+	for t := range tweets {
+		if t.IsTalkingAboutGo() {
+			fmt.Println(t.Username, "\ttweets about golang")
+		} else {
+			fmt.Println(t.Username, "\tdoes not tweet about golang")
 		}
 	}
 }
 
 func main() {
 	start := time.Now()
-	tweets := make(chan *Tweet)
-	quit := make(chan bool)
+	tweets := make(chan *Tweet, 10)
 	var wg sync.WaitGroup
 
 	// Producer
 	wg.Add(1)
-	go producer(GetMockStream(), tweets, quit, &wg)
+	go producer(GetMockStream(), tweets, &wg)
 
 	// Consumer
 	wg.Add(1)
-	go consumer(tweets, quit, &wg)
+	go consumer(tweets, &wg)
+
 	wg.Wait()
 
 	fmt.Printf("Process took %s\n", time.Since(start))
